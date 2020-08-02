63c399bb02953af10cf4ac5619eb2d9a11d243a6
Eduan Bekker <bekker.eduan@gmail.com>
2020-08-02 18:36:19 +0200

    Implemented formatter to solve LOT_SIZE error when making an order with too much decimal places

--- src/main/kotlin/com/eduanbekker/cryptotrader/exchange/binance/BinanceConfig.kt
+++ src/main/kotlin/com/eduanbekker/cryptotrader/exchange/binance/BinanceConfig.kt
@@ -2,8 +2,13 @@ package com.eduanbekker.cryptotrader.exchange.binance
 
 import com.binance.api.client.BinanceApiClientFactory
 import com.binance.api.client.BinanceApiRestClient
+import com.binance.api.client.domain.general.FilterType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
 
 @Configuration
 @ConditionalOnBinance
@@ -13,5 +18,19 @@ class BinanceConfig {
 			BinanceApiClientFactory.newInstance(binanceCredentials.key, binanceCredentials.secret).newRestClient()
 
 	@Bean
-	fun binanceExchangeApi(binanceApiRestClient: BinanceApiRestClient) = BinanceExchange(binanceApiRestClient)
+	fun binanceExchangeApi(binanceApiRestClient: BinanceApiRestClient, decimalConverter: (String) -> NumberFormat) =
+			BinanceExchange(binanceApiRestClient, decimalConverter)
+
+	@Bean
+	fun decimalConverter(binanceApiRestClient: BinanceApiRestClient): (String) -> NumberFormat = { symbol: String ->
+		binanceApiRestClient.exchangeInfo.symbols
+				.first { it.symbol == symbol }
+				.getSymbolFilter(FilterType.LOT_SIZE)
+				.stepSize
+				.substringBefore("1")
+				.replace("0", "#")
+				.plus("#")
+				.let { DecimalFormat.getInstance(Locale.US) }
+				.also { it.roundingMode = RoundingMode.DOWN }
+	}
 }
--- src/main/kotlin/com/eduanbekker/cryptotrader/exchange/binance/BinanceExchange.kt
+++ src/main/kotlin/com/eduanbekker/cryptotrader/exchange/binance/BinanceExchange.kt
@@ -4,8 +4,16 @@ import com.binance.api.client.BinanceApiRestClient
 import com.binance.api.client.domain.account.NewOrder
 import com.binance.api.client.domain.account.NewOrderResponseType
 import com.eduanbekker.cryptotrader.exchange.ExchangeApi
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.text.NumberFormat
 
-class BinanceExchange(private val binanceApiRestClient: BinanceApiRestClient) : ExchangeApi {
+class BinanceExchange(
+		private val binanceApiRestClient: BinanceApiRestClient,
+		private val converter: (String) -> NumberFormat
+) : ExchangeApi {
+	val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
 	override fun getName() = "Binance"
 
 	override fun getPrice(symbol: String) =
@@ -17,13 +25,15 @@ class BinanceExchange(private val binanceApiRestClient: BinanceApiRestClient) : 
 	override fun getSymbols() = binanceApiRestClient.exchangeInfo.symbols.map { it.symbol }
 
 	override fun marketBuyOrder(symbol: String, quantity: Double): Long =
-			binanceApiRestClient.newOrder(NewOrder.marketBuy(symbol, quantity.toString())
+			binanceApiRestClient.newOrder(NewOrder.marketBuy(symbol, convertAndLog(symbol, quantity))
 					.newOrderRespType(NewOrderResponseType.FULL))
 					.orderId
 
 	override fun marketSellOrder(symbol: String, quantity: Double): Long =
-			binanceApiRestClient.newOrder(NewOrder.marketSell(symbol, quantity.toString())
+			binanceApiRestClient.newOrder(NewOrder.marketSell(symbol, convertAndLog(symbol, quantity))
 					.newOrderRespType(NewOrderResponseType.FULL))
 					.orderId
 
+	private fun convertAndLog(symbol: String, quantity: Double) =
+			converter(symbol).format(quantity).also { logger.info("Converted $quantity to $it") }
 }
